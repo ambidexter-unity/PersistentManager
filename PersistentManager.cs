b19f44d7cefd247321d476d2de557e3658e2c812
aaamarkin <aaamarkin@gmail.com>
2019-06-21 18:27:48 +0300

    Initial commit

---
+++ PersistentManager.cs
@@ -0,0 +1,202 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Common.Service;
+using UniRx;
+using UnityEngine;
+using UnityEngine.Assertions;
+using Zenject;
+
+namespace Common.PersistentManager
+{
+	[DisallowMultipleComponent]
+	public class PersistentManager : MonoInstaller<PersistentManager>, IPersistentManager
+	{
+		private enum StorageType
+		{
+			All,
+			File,
+			PlayerPrefs
+		}
+
+		public const string PersistentKey = "good_night_tamafish";
+		public static string SavedDataPath => Path.Combine(Application.persistentDataPath, "savedata.json");
+
+		private readonly BoolReactiveProperty _ready = new BoolReactiveProperty(false);
+
+		// Вспомогательные классы для сериализации записей сохраняемыж данных.
+		[Serializable]
+		private class RawDataRecord
+		{
+			// ReSharper disable InconsistentNaming
+			public string key;
+
+			public string value;
+			// ReSharper restore InconsistentNaming
+		}
+
+		[Serializable]
+		private class RawData
+		{
+			// ReSharper disable InconsistentNaming
+			public List<RawDataRecord> rawData = new List<RawDataRecord>();
+			// ReSharper restore InconsistentNaming
+		}
+		//---------------------------------------//
+
+		private readonly RawData _rawPlayerPrefsData = new RawData();
+		private readonly RawData _rawFileData = new RawData();
+		private bool _isValid;
+
+		public override void InstallBindings()
+		{
+			Container.Bind<IPersistentManager>().FromInstance(this);
+		}
+
+		// IGameService
+
+		void IGameService.Initialize()
+		{
+			RestoreGameData();
+			_ready.SetValueAndForceNotify(true);
+		}
+
+		public IReadOnlyReactiveProperty<bool> Ready => _ready;
+
+		// \IGameService		
+
+		private void OnApplicationFocus(bool hasFocus)
+		{
+			if (hasFocus) return;
+			PersistGameData(StorageType.All);
+		}
+
+		// IPersistentManager
+
+		public void Persist<T>(T data, bool asPlayerPrefs = false, bool lazy = false) where T : IPersistent<T>, new()
+		{
+			Assert.IsTrue(Ready.Value);
+
+			if (asPlayerPrefs)
+			{
+				_rawPlayerPrefsData.rawData.RemoveAll(record => record.key == data.PersistentId);
+				_rawPlayerPrefsData.rawData.Add(new RawDataRecord
+				{
+					key = data.PersistentId,
+					value = JsonUtility.ToJson(data)
+				});
+			}
+			else
+			{
+				_rawFileData.rawData.RemoveAll(record => record.key == data.PersistentId);
+				_rawFileData.rawData.Add(new RawDataRecord {key = data.PersistentId, value = JsonUtility.ToJson(data)});
+			}
+
+			_isValid = false;
+			if (!lazy) PersistGameData(asPlayerPrefs ? StorageType.PlayerPrefs : StorageType.File);
+		}
+
+		public bool Restore<T>(T data) where T : IPersistent<T>, new()
+		{
+			Assert.IsTrue(Ready.Value);
+
+			var rawData = _rawFileData.rawData.SingleOrDefault(record => record.key == data.PersistentId) ??
+			              _rawPlayerPrefsData.rawData.SingleOrDefault(record => record.key == data.PersistentId);
+			if (rawData == null)
+			{
+				return false;
+			}
+
+			data.Restore(JsonUtility.FromJson<T>(rawData.value));
+			return true;
+		}
+
+		public T GetPersistentValue<T>() where T : IPersistent<T>, new()
+		{
+			Assert.IsTrue(Ready.Value);
+
+			var res = new T();
+			Restore(res);
+			return res;
+		}
+
+		public bool Remove<T>(T data) where T : IPersistent<T>, new()
+		{
+			Assert.IsTrue(Ready.Value);
+
+			if (data == null)
+			{
+				return false;
+			}
+
+			return Remove(data.PersistentId);
+		}
+
+		public bool Remove(string id)
+		{
+			Assert.IsTrue(Ready.Value);
+
+			var res = false;
+			if (_rawFileData.rawData.RemoveAll(record => record.key == id) > 0)
+			{
+				_isValid = false;
+				PersistGameData(StorageType.File);
+				res = true;
+			}
+
+			if (_rawPlayerPrefsData.rawData.RemoveAll(record => record.key == id) > 0)
+			{
+				_isValid = false;
+				PersistGameData(StorageType.PlayerPrefs);
+				res = true;
+			}
+
+			return res;
+		}
+
+		// \IPersistentManager
+
+		private void RestoreGameData()
+		{
+			var path = SavedDataPath;
+			if (File.Exists(path))
+			{
+				JsonUtility.FromJsonOverwrite(File.ReadAllText(path), _rawFileData);
+			}
+			else
+			{
+				_rawFileData.rawData.Clear();
+			}
+			
+			if (PlayerPrefs.HasKey(PersistentKey))
+			{
+				JsonUtility.FromJsonOverwrite(PlayerPrefs.GetString(PersistentKey), _rawPlayerPrefsData);
+			}
+			else
+			{
+				_rawPlayerPrefsData.rawData.Clear();
+			}
+
+			_isValid = true;
+		}
+
+		private void PersistGameData(StorageType storageType)
+		{
+			if (_isValid) return;
+
+			if (storageType == StorageType.PlayerPrefs || storageType == StorageType.All)
+			{
+				PlayerPrefs.SetString(PersistentKey, JsonUtility.ToJson(_rawPlayerPrefsData));
+				PlayerPrefs.Save();
+			}
+
+			if (storageType == StorageType.File || storageType == StorageType.All)
+			{
+				File.WriteAllText(SavedDataPath, JsonUtility.ToJson(_rawFileData));
+			}
+
+			_isValid = true;
+		}
+	}
+}
